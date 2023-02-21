c7d77b2dd0610228cadc31a696fa7938d296a1e2
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-21 11:41:18 +0300

    Create TodosList component

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,12 @@
+const TodosList = (props) => {
+  const { todosProps } = props;
+  return (
+    <ul>
+      {todosProps.map((todo) => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodosList;
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,9 +1,30 @@
 import InputTodo from './InputTodo';
+import TodosList from './TodosList';
 
-const TodosLogic = () => (
-  <>
-    <InputTodo />
-  </>
-);
+const TodosLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
+  return (
+    <>
+      <InputTodo />
+      <TodosList todosProps={todos} />
+    </>
+  );
+};
 
 export default TodosLogic;
