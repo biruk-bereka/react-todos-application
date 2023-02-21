fd14fcda3a7f88fe12f0b3f19d977bcfbac76deb
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-21 16:10:37 +0300

    Implement function  to update state of todos when toggling checkbox

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 
-const TodoItem = ({ todo }) => (
-  <li>{todo.title}</li>
+const TodoItem = ({ todo, handleChange }) => (
+  <li>
+    <input type="checkbox" checked={todo.completed} onChange={() => handleChange(todo.id)} />
+    {todo.title}
+  </li>
 );
 
 TodoItem.propTypes = {
   todo: PropTypes.shape({
     title: PropTypes.string,
-    id: PropTypes.string,
+    id: PropTypes.number,
     completed: PropTypes.bool,
   }),
+  handleChange: PropTypes.func.isRequired,
 };
 
 TodoItem.defaultProps = { todo: {} };
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -3,24 +3,19 @@ import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
 const TodosList = (props) => {
-  const { todosProps } = props;
+  const { todosProps, handleChange } = props;
   return (
     <ul>
       {todosProps.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} handleChange={handleChange} />
       ))}
     </ul>
   );
 };
 
 TodosList.propTypes = {
-  todosProps: PropTypes.shape(
-    [],
-  ),
-};
-
-TodosList.defaultProps = {
-  todosProps: [],
+  todosProps: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default TodosList;
